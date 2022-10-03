446131e7def49246a274833d0e3c8b53953231c3
peter marie <peterogwara@gmail.com>
2022-10-03 12:30:40 +0100

    player manager storage algorithm update

--- contracts/peemotestplayermanager.sol
+++ contracts/peemotestplayermanager.sol
@@ -4,32 +4,67 @@ pragma solidity >=0.6.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PeemoTestRewardManager is Ownable {
+contract PeemoTestPlayerManager is Ownable {
     address token;
     address stake_manager;
     address unclaimed_peem_holder;
+    uint player_count;
 
     //User mappings
+    struct Players {
+        uint player_id;
+        string hashed_password;
+        string username;
+        string email;
+        uint8 verifiedstatus;
+        uint unclaimedpeem;
+    }
+    mapping(uint=>Players) playeridtodetails;
+    mapping(string=>uint) playeremailtoid;
+
     mapping(uint=>address) playeridtoaddress;
     mapping(uint=>string) playeridtousername;
     mapping(uint=>string) playeridtoemail;
     mapping(uint=>uint8) playeridtoverifiedstatus;
     mapping(uint=>uint) playeridtounclaimedpeem;
+    enum VERIFIED_STATUS {
+        NONE,
+        EMAIL_VERIFIED,
+        ADDRESS_VERIFIED
+    }
 
-    constructor(address _token){
+    constructor(address _token, address _unclaimed_peem_holder){
         token = _token;
+        unclaimed_peem_holder = _unclaimed_peem_holder;
     }
 
     function addPlayer(string memory _email, string memory _username) public onlyOwner {
-        
+        VERIFIED_STATUS verified_status;
+        verified_status = VERIFIED_STATUS.NONE;
+        uint new_player_id = player_count;
+        Players memory new_player = Players(new_player_id, 'password', _username, _email, uint8(verified_status), 0);
+        playeridtodetails[new_player_id] = new_player;
+        player_count = player_count + 1;
     }
 
     /**verifyPlayer transfers all unclaimed peem of a player from the unclaimed peem address to the player's verified address */
-    function verifyPlayer(uint _playerId, address _playerAddress) public onlyOwner {
+    function verifyPlayerAddress(uint _playerId, address _playerAddress) public onlyOwner {
+
+    }
+
+    function verifyPlayer(uint _playerId) public {
 
     }
 
     function isVerified(uint _playerId) public view returns(uint8){
-        return playeridtoverifiedstatus[_playerId];
+        return playeridtodetails[_playerId].verifiedstatus;
+    }
+
+    function getPlayerId(string calldata _email) public view returns(uint){
+        return playeremailtoid[_email];
+    }
+
+    function getPlayerUsername(uint _playerId) public view returns(string memory){
+        return playeridtodetails[_playerId].username;
     }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,18 @@
-from brownie import PeemoTester, PeemoTestRewardManager
+#!C:/Users/TheresaOgwara/AppData/Local/Programs/Python/Python310/python
+
+from audioop import add
+from brownie import PeemoTester, PeemoTestStakeManager, PeemoTestPlayerManager
 import scripts.functions as func
 from web3 import Web3
 
-player_id = 1
+player_account = 0
+
+account = func.get_account()
+unclaimed_peem_address = func.get_account(index=2)
 
 def deploy_peem():
     if(len(PeemoTester) <= 0):
         initial_supply = Web3.toWei(10000000000, 'ether')
-        account = func.get_account()
         contract = PeemoTester.deploy(initial_supply, {'from': account})
     else:
         contract = PeemoTester[-1]
@@ -15,34 +20,50 @@ def deploy_peem():
 
 def deploy_staking_contract():
     peem_contract = deploy_peem()
-    if(len(PeemoTestRewardManager) <= 0):
+    if(len(PeemoTestStakeManager) <= 0):
         token_address = peem_contract.address
-        account = func.get_account()
-        contract = PeemoTestRewardManager.deploy(token_address, {'from': account})
+        contract = PeemoTestStakeManager.deploy(token_address, {'from': account})
     else:
-        contract = PeemoTestRewardManager[-1]
+        contract = PeemoTestStakeManager[-1]
     return contract
 
+def deploy_player_contract():
+    peem_contract = deploy_peem()
+    if(len(PeemoTestPlayerManager) <= 0):
+        token_address = peem_contract.address
+        contract = PeemoTestPlayerManager.deploy(token_address, unclaimed_peem_address, {'from': account})
+    else:
+        contract = PeemoTestPlayerManager[-1]
+    return contract
+
+def get_player_details():
+    playercontract = deploy_player_contract()
+    player_id = playercontract.getPlayerId('peterogwara@gmail.com', {'from': account})
+    verified_status = playercontract.isVerified(player_id, {"from": account})
+    username = playercontract.getPlayerUsername(player_id, {"from": account})
+    if(verified_status == 0):
+        player_account = unclaimed_peem_address
+    else:
+        player_account = playercontract.getplayerAddress(player_id, {"from": account})
+    return [verified_status, player_account, username]
+
 def get_peem():
-    account = func.get_account()
     contract = deploy_peem()
-    receiving_account = func.get_account(index=1)
     amount = int(input("Enter amount to Receive/n"))
-    txn = contract.transfer(receiving_account, amount, {"from": account})
+    txn = contract.transfer(player_account, amount, {"from": account})
     txn.wait(1)
     return amount
 
 def stake_peem():
-    account = func.get_account(index=1)
     peem_available = get_peem()
     amount_to_stake = int(input("Enter amount to Stake/n"))
     contract = deploy_staking_contract()
     peem_contract = deploy_peem()
-    txn = peem_contract.increaseAllowance(contract.address, amount_to_stake, {"from":account})
+    txn = peem_contract.increaseAllowance(contract.address, amount_to_stake, {"from":player_account})
     txn.wait(1)
-    txn1 = contract.stakeToken(amount_to_stake, {"from": account})
+    txn1 = contract.stakeToken(player_id, amount_to_stake, {"from": account})
     txn1.wait(1)
-    print(contract.getStake({"from": account}))
+    print(contract.getStake(player_id, {"from": account}))
 
 
 def withdraw_staked_peem():
@@ -52,5 +73,13 @@ def get_staked_peem():
     contract = deploy_staking_contract()
     staked_peem = contract.getPlayerStake(player_id)
 
+def add_player():
+    contract = deploy_player_contract()
+    txn = contract.addPlayer('peterogwara@gmail.com', 'PeterMarie', {'from': account})
+    txn.wait(1)
+    print('Player Added')
+
 def main():
-    stake_peem()
+    add_player()
+    player_details = get_player_details()
+    print(player_details)
