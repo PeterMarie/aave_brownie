e6b16fd595e33d25b3841dd30e230003ed455cda
Peter Ogwara <peterogwara@gmail.com>
2023-03-17 09:17:19 +0100

    change

--- contracts/peemotestplayermanager.sol
+++ contracts/peemotestplayermanager.sol
@@ -68,3 +68,42 @@ contract PeemoTestPlayerManager is Ownable {
         return playeridtodetails[_playerId].username;
     }
 }
+
+contract Tester{
+    
+    uint userCount;
+    string[] _techstack;
+    CatDetails[] _cats;
+
+    struct People {
+        string name;
+        uint height_in_cm;
+        bytes gender;
+        string[] tech_stack;
+        CatDetails[] my_cats;
+    }
+    struct CatDetails {
+        string name;
+        uint age;
+        string color;
+    }
+
+    mapping (uint=>People) user_id_to_details;
+
+    function setUser() public {
+        uint userId = userCount++;
+        string[] storage my_techstack = _techstack;
+        my_techstack.push("Python");
+        my_techstack.push("JavaScript");
+        my_techstack.push("Solidity");
+        CatDetails[] storage cats = _cats;
+        CatDetails memory firstcat = CatDetails("Snuffles", 3, 'yellow');
+        cats.push(firstcat);
+        People memory yours_truly = People("Peter M. Ogwara", 155, "m", my_techstack, cats);
+        user_id_to_details[userId] = yours_truly;
+    }
+
+    function getCat() public view returns(string memory){
+        return user_id_to_details[1].my_cats[1].name;
+    }
+}
